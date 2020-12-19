4bd561be8c84b29da2b817e5fdca40208aaf2327
Ron Welch <mmcelt@yahoo.com>
2020-12-19 13:56:16 -0700

    10 - Steering the Car

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -7,10 +7,15 @@ public class CarController : MonoBehaviour
 	#region Fields
 
 	[SerializeField] Rigidbody _theRB;
+
 	[SerializeField] float _maxSpeed;
 	[SerializeField] float _forwardAccel = 8f, _reverseAccel = 4f;
 
 	float _speedInput;
+
+	[SerializeField] float _turnStrength = 180f;
+	float _turnInput;
+
 	#endregion
 
 	#region MonoBehaviour Methods
@@ -34,12 +39,24 @@ public class CarController : MonoBehaviour
 			_speedInput = Input.GetAxis("Vertical") * _reverseAccel;
 		}
 
+		_turnInput = Input.GetAxis("Horizontal");
+
+		if (Input.GetAxis("Vertical") != 0)
+		{
+			transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(
+				0f, 
+				_turnInput * _turnStrength * Time.deltaTime, 
+				0f));
+		}
+
+
+
 		transform.position = _theRB.transform.position;
 	}
 
 	void FixedUpdate() 
 	{
-		_theRB.AddForce(new Vector3(0f, 0f, _speedInput * 1000f));
+		_theRB.AddForce(transform.forward * _speedInput * 1000f);
 	}
 	#endregion
 
