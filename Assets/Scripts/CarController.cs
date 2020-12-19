36fc926003ab707420f9cd522ee34ff3ffe68e13
Ron Welch <mmcelt@yahoo.com>
2020-12-19 10:41:11 -0700

    6 - First Script Basics

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] float _maxSpeed;
+
+	[SerializeField] Rigidbody _theRB;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
