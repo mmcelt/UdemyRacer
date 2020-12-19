78635a0f3da1eef7dc70dca421289bbe54149693
Ron Welch <mmcelt@yahoo.com>
2020-12-19 11:44:18 -0700

    8 - Controlling Acceleration

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,24 +6,40 @@ public class CarController : MonoBehaviour
 {
 	#region Fields
 
-	[SerializeField] float _maxSpeed;
-
 	[SerializeField] Rigidbody _theRB;
+	[SerializeField] float _maxSpeed;
+	[SerializeField] float _forwardAccel = 8f, _reverseAccel = 4f;
 
+	float _speedInput;
 	#endregion
 
 	#region MonoBehaviour Methods
 
 	void Start() 
 	{
+
 		_theRB.transform.parent = null;
 	}
-	
-	void Update() 
+
+	void Update()
 	{
-		_theRB.AddForce(new Vector3(0f,0f,100f));
+		_speedInput = 0f;
+
+		if (Input.GetAxis("Vertical") > 0)
+		{
+			_speedInput = Input.GetAxis("Vertical") * _forwardAccel;
+		}
+		else if(Input.GetAxis("Vertical") < 0)
+		{
+			_speedInput = Input.GetAxis("Vertical") * _reverseAccel;
+		}
 
 		transform.position = _theRB.transform.position;
+	}
+
+	void FixedUpdate() 
+	{
+		_theRB.AddForce(new Vector3(0f,0f,_speedInput));
 	}
 	#endregion
 
