672bd6c3c52db8907aae646fc5b66b2bf3e9720e
Ron Welch <mmcelt@yahoo.com>
2020-12-19 11:24:48 -0700

    7 - Making the Car Move

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -16,12 +16,14 @@ public class CarController : MonoBehaviour
 
 	void Start() 
 	{
-		
+		_theRB.transform.parent = null;
 	}
 	
 	void Update() 
 	{
-		
+		_theRB.AddForce(new Vector3(0f,0f,100f));
+
+		transform.position = _theRB.transform.position;
 	}
 	#endregion
 
